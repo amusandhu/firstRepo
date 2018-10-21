eed46b599f382d37b156f90ab67dfeefd243efde
aman <amnkhokhar@gmail.com>
2018-10-20 23:15:40 -0400

    forloop added

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -11,6 +11,11 @@ namespace ConsoleApp5
         static void Main(string[] args)
         {
             Console.WriteLine("GitHub");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("value of I={0}",i);
+            }
+            Console.ReadLine();
         }
     }
 }
